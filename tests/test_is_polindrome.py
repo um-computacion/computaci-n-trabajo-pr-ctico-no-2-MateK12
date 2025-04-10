b5551af74c9127f107cf859421bff12cce5ceff6
Mateo <kristichmateo@gmail.com>
2025-04-10 20:45:58 -0300

    Agregar tests para casos no palíndromos

--- tests/test_is_polindrome.py
+++ tests/test_is_polindrome.py
@@ -10,6 +10,16 @@ class Test_is_palindrome(unittest.TestCase):
     def polindrom_phrase_1(self):
         self.assertEqual(is_palindrome('Anita lava la tina'),True)
     def polindrom_phrase_2(self):
-        self.assertEqual(is_palindrome('Hola mundo como anda'),False)
+        self.assertEqual(is_palindrome('Yo hago yoga hoy'),False)
     def polindrom_phrase_3(self):
         self.assertEqual(is_palindrome('Aman a Panama'),True)
+    def no_palindrome_1(self):
+        self.assertEqual(is_palindrome('Hola mundo como anda'),False)
+    def no_palindrome_2(self):
+        self.assertEqual(is_palindrome('botella'),False)
+    def no_palindrome_3(self):
+        self.assertEqual(is_palindrome('rusia'),False)
+
+
+
+        
