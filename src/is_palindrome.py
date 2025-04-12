02bedea8f0934cd920a55b999f9f9fa5735b96c8
Mateo <kristichmateo@gmail.com>
2025-04-12 15:41:38 -0300

    tp terminado

--- src/is_palindrome.py
+++ src/is_palindrome.py
@@ -1,8 +1,8 @@
 def is_palindrome(word):
-
+    if len(word) <= 1:
+        return False
     word = word.replace(':','').replace(',','').replace('.','').replace('?','').replace("'",'').replace(" ",'').lower()
     reverse_word = word[::-1]
-
-
-word = input('palindromo??')
-print(is_palindrome(word))
+    if reverse_word == word:
+        return True 
+    else: return False
